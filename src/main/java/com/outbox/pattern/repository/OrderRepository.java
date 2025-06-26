e3583f828b7e8c7bce1b33f520c31b04ffd37f4a
msaad10 <saad.muhammad@tenpearls.com>
2025-06-26 15:11:43 +0500

    remove unused  import

--- src/main/java/com/outbox/pattern/repository/OrderRepository.java
+++ src/main/java/com/outbox/pattern/repository/OrderRepository.java
@@ -2,7 +2,6 @@ package com.outbox.pattern.repository;
 
 import com.outbox.pattern.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     // Custom query methods can be defined here if needed
